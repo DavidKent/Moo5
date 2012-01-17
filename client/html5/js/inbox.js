5a3898685087f37566950d47c4fcc8024f98c529
DavidKent <desperado1234@live.com>
2012-01-17 17:20:16 -0500

    Javascript added to inbox

---
+++ client/html5/js/inbox.js
@@ -0,0 +1,89 @@
+var message_code, messages = [];
+function get_code(){
+	$.ajax({
+  url: 'modules/message.html',
+  beforeSend: function( xhr ) {
+    xhr.overrideMimeType( 'text/plain; charset=x-user-defined' );
+  },
+  success: function(data) {
+    message_code = data;
+    console.log(message_code);
+	setup_messages();
+	$('.well').click(message_onClick);
+  }
+});
+}
+
+function Message (args)
+{
+	this.select = function(){
+		for(var i = 0; i < messages.length; i++)
+		{
+			if(messages[i].id != this.id)
+				messages[i].hide();
+			else
+				messages[i].show();
+			
+		}
+	}
+	this.hide = function(start){
+		var time = start?0:300;
+		$("#"+this.id).find('.top_desc').fadeIn(time);
+		$("#"+this.id).find('.message_body').hide(time);
+	}
+	this.show = function(){
+		$("#"+this.id).find('.top_desc').fadeOut(300, function(){
+			$("#"+this.id).find('.message_body').show(300);
+		});
+		
+	}
+	this.id = args.id;
+	this.html;
+	this.output = function(){
+		if(message_code == undefined)
+			get_code();
+		this.html = message_code.replace('{|message|}', args.message)
+						   .replace('{|user|}', args.user)
+						   .replace('{|id|}', args.id)
+						   .replace(/\{\|description\|\}/g, args.description);
+		$(".hero-unit").html(this.html+$(".hero-unit").html());
+		this.hide(true);
+	}
+	this.add = function(){
+		messages.push(this);
+		this.output();
+	}
+	
+}
+function setup_messages(){
+	var m = new Message ({
+		id:"asbasd", 
+		description:"Test Description", 
+		message:"test Message hi asdasd asdijasdjisjdiasjdiasdj",
+		user: "batman"}
+	);
+	m.add();
+	 m = new Message ({
+		id:"asbasd1", 
+		description:"Test Description", 
+		message:"test Message hi asdasd asdijasdjisjdiasjdiasdj",
+		user: "batman"}
+	);
+	m.add();
+	 m = new Message ({
+		id:"asbasd2", 
+		description:"Test Description", 
+		message:"test Message hi asdasd asdijasdjisjdiasjdiasdj",
+		user: "batman"}
+	);
+	m.add();
+}
+function message_onClick(e){
+	var h = $(this).attr('id');
+	for(var i = 0; i < messages.length; i++)
+	{
+		if(messages[i].id == h)
+			messages[i].select();
+	}
+}
+get_code();
