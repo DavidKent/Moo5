1f7719f0dc777dff5c35ffe0485ff014b2f7ba19
unknown <Zerotension@Zerotension-PC.(none)>
2012-01-17 19:18:47 -0500

    Updated tabs to spaces

--- server/utils/key_hook.js
+++ server/utils/key_hook.js
@@ -1,6 +1,6 @@
 var cons = require('./console.js');
 var com = require('./commands.js');
-	exports.setHook = function(){
-		stdin = process.openStdin();
-		stdin.on('data', function(inp) { var u = ""+inp; com.parseInput(u); });
-	};
+    exports.setHook = function(){
+        stdin = process.openStdin();
+        stdin.on('data', function(inp) { var u = ""+inp; com.parseInput(u); });
+    };
--- server/utils/request.js
+++ server/utils/request.js
@@ -5,30 +5,30 @@ var requests = 0;
 var tests = false;
 
 exports.getClientIp = function(request){
-	return request.connection.remoteAddress;
-	};
-	
+    return request.connection.remoteAddress;
+    };
+    
 exports.connection = function(request, result){
-		var h = url.parse(request.url).href, ct = moon.getContentType(h);
-		if(logRequest(h))
-			update(request);
-		if(tests)
-			moon.test();
-		result.writeHead(200, {'Content-Type': ct});
-		if(moon.getPage(h) == undefined)
-			result.end(moon.getPage('/register.html'));
-		result.end(moon.getPage(h));
-	};
-	function update(request) {
+        var h = url.parse(request.url).href, ct = moon.getContentType(h);
+        if(logRequest(h))
+            update(request);
+        if(tests)
+            moon.test();
+        result.writeHead(200, {'Content-Type': ct});
+        if(moon.getPage(h) == undefined)
+            result.end(moon.getPage('/register.html'));
+        result.end(moon.getPage(h));
+    };
+    function update(request) {
 
-		requests++;
-		cons.info("Connection["+requests+"] request recieved at "+ exports.getClientIp(request));
-	}
+        requests++;
+        cons.info("Connection["+requests+"] request recieved at "+ exports.getClientIp(request));
+    }
 exports.getRequests = function(){
-		return requests;
-	};
-	function logRequest(href){
-		if(href.indexOf('html') == -1)
-		return false;
-	 return true;
-	}
+        return requests;
+    };
+    function logRequest(href){
+        if(href.indexOf('html') == -1)
+        return false;
+     return true;
+    }
